89ebe46282d19d52935e44066742c2c62f6e8c49
adibg17 <adityabg11@gmail.com>
2024-02-18 22:25:00 +0530

    ADDED SUBTRACT FUNCTION

--- add.c
+++ add.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    int a,b,add;
+    int a,b,add,sub;
     a=10;
     b=15;
     add=a+b;
+    sub=a-b;
     printf("ADDIITON IS %d",add);
+    printf("SUBTRACTION IS %d",sub);
 }
